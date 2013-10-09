a02b2ea540d4995ff0b6d2edf22e7e2d9049c999
Sanyk28 <san-heng-yi-shu@163.com>
2013-10-09 12:27:44 +1100

    review make

---
+++ 1/classes_objects/practice/className.py
@@ -0,0 +1,10 @@
+class className:
+
+	def createName(self, name):
+		self.name = name
+	def displayName(self):
+		return self.name
+	def saying(self):
+		print 'hello %s' %self.name
+
+
---
+++ 1/classes_objects/practice/try.py
@@ -0,0 +1,7 @@
+from className import className
+first=className()
+second=className()
+first.createName('Jody')
+second.createName('Silly')
+print first.displayName()
+second.saying()
